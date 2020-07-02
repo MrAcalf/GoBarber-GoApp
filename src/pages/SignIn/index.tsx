ee39a2b7c1f722e5ab3f335a2fda31ea6e1ca150
mracalf <antonio.lima16@fatec.sp.gov.br>
2020-07-02 19:10:52 -0300

    SignIn and SignOut hooks are working and saving the user and token into browser local storage.

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,34 +1,79 @@
-import React from 'react';
+import React, { useRef, useCallback } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Container, Content, Background } from './styles';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+
+import { useAuth } from '../../hooks/AuthContext';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.svg';
 
-const SignIn: React.FC = () => (
-  <Container>
-    <Content>
-      <img src={logoImg} alt="Go Barber" />
-      <form>
-        <h1>Faça seu logon</h1>
+interface SignInFormData {
+  email: string;
+  password: string;
+}
 
-        <Input name="email" icon={FiMail} placeholder="E-mail" />
+const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
 
-        <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
+  const { signIn } = useAuth();
 
-        <Button type="submit">Entrar</Button>
+  const handleSubmit = useCallback(async(data: SignInFormData) => {
+    try {
+      formRef.current?.setErrors({});
 
-        <a href="forgot">Esqueci minha senha</a>
-      </form>
-      <a href="/">
-        <FiLogIn />
-        Criar Conta
-      </a>
-    </Content>
-    <Background />
-  </Container>
-);
+      const schema = Yup.object().shape({
+        email: Yup
+          .string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup
+          .string()
+          .required('Senha obrigatória')
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      signIn({
+        email: data.email,
+        password: data.password
+      });
+    } catch (err) {
+      const errors = getValidationErrors(err);
+
+      formRef.current?.setErrors(errors);
+    }
+  }, [signIn]);
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="Go Barber" />
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Faça seu logon</h1>
+
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+
+          <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
+
+          <Button type="submit">Entrar</Button>
+
+          <a href="forgot">Esqueci minha senha</a>
+        </Form>
+        <a href="/">
+          <FiLogIn />
+          Criar Conta
+        </a>
+      </Content>
+      <Background />
+    </Container>
+  );
+};
 
 export default SignIn;
